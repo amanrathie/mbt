99bf340eca641b7925357d49d5f3c7bcad1f6fb3
Aman Rathie <amanrathie@gmail.com>
2018-02-01 00:01:45 -0200

    Exemplo de teste de integracao

---
+++ src/test/java/br/gov/cgu/mbt/integracao/BootIntegracaoTest.java
@@ -0,0 +1,83 @@
+package br.gov.cgu.mbt.integracao;
+
+import static io.github.seleniumquery.SeleniumQuery.$;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import br.gov.cgu.mbt.Application;
+import io.github.seleniumquery.SeleniumQuery;
+
+public abstract class BootIntegracaoTest {
+    private final Logger logger = LoggerFactory.getLogger(BootIntegracaoTest.class);
+
+    private static ConfigurableApplicationContext applicationContext;
+
+    static {
+        prepararDriverSelenium();
+        startSpringBoot();
+    }
+
+    private static void startSpringBoot() {
+        System.setProperty("spring.profiles.active", "http,h2");
+        applicationContext = SpringApplication.run(Application.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    private static void prepararDriverSelenium() {
+        System.setProperty("webdriver.chrome.driver",
+                SystemUtils.IS_OS_WINDOWS ? "target/test-classes/chromedriver_win32.exe" : "target/test-classes/chromedriver_linux64");
+//        System.setProperty("webdriver.chrome.logfile", "c:\\tmp\\chromedriver.log");
+//        System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
+        SeleniumQuery.$.driver().use(driver);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> $.driver().quit()));
+    }
+
+    /*
+    Alguns testes estão rodando mais rapido que o hibernate na hora de salvar a entidade, aí o teste falha.
+    */
+    public static void refreshSeDer404(String urlDestino) {
+        WebDriverWait wait = new WebDriverWait($.driver().get(), 10);
+        wait.until(ExpectedConditions.urlContains(urlDestino));
+        if ($(".e404").size() > 0) {
+            $.url($.url() + "&refresh=aaa");
+        }
+    }
+
+    /*@Before
+    public void resetarDataSourceEntreOsTestes() throws Exception {
+        logger.info("------------------- RESETANDO DADOS DO H2 ------------------------------");
+        DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+        DatabasePopulator populator = new ResourceDatabasePopulator(
+                resourceLoader.getResource("classpath:sql/reset.sql"),
+                resourceLoader.getResource("classpath:sql/esquema.sql"),
+                resourceLoader.getResource("classpath:sql/dados.sql")
+        );
+
+        DatabasePopulatorUtils.execute(populator, BancoH2AppConfig.datasource);
+
+        Cache cache = applicationContext.getBean(SessionFactory.class).getCache();
+        cache.evictAllRegions();
+    }*/
+
+    /**
+     * Testa se a URL contém a string
+     */
+    protected void assertUrlContains(String s) {
+        assertThat($.url(), containsString(s));
+    }
+
+}
---
+++ src/test/java/br/gov/cgu/mbt/integracao/TestandoIntegracaoTest.java
@@ -0,0 +1,20 @@
+package br.gov.cgu.mbt.integracao;
+
+import static io.github.seleniumquery.SeleniumQuery.$;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+
+public class TestandoIntegracaoTest extends BootIntegracaoTest {
+	
+	public static String URL_BASE = "http://localhost:8080/";
+
+    @Test
+    public void estado_inicial_da_tela() throws Exception {
+    	$.url(URL_BASE);
+    	
+    	assertThat($("h1").html()).isEqualTo("Ola Mapas Brasil");
+    }
+    
+}
