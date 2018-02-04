c5c7a5e9a34193fba3c2eb313d92b516bc6a2557
Aman Rathie <amanrathie@gmail.com>
2018-02-04 19:21:27 -0200

    Excluindo classes do projeto exemplo

--- src/main/java/br/gov/cgu/mbt/aplicacao/email/EmailNaoEnviadoException.java
+++
@@ -1,9 +0,0 @@
-package br.gov.cgu.mbt.aplicacao.email;
-
-public class EmailNaoEnviadoException extends RuntimeException {
-
-    public EmailNaoEnviadoException(Exception e) {
-        super("Não foi possível enviar um e-mail.", e);
-    }
-
-}
