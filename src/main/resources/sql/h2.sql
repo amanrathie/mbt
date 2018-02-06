29f6a2478c3d090b9482707267e949b9d012e2f9
Aman Rathie <amanrathie@gmail.com>
2018-02-06 18:22:45 -0200

    Refactoring

--- src/main/resources/sql/h2.sql
+++ src/main/resources/sql/h2.sql
@@ -1,2 +1,2 @@
-RUNSCRIPT FROM 'file:src/main/resources/db/migration/V1__tabelas_e_dados_iniciais_H2.sql';
+RUNSCRIPT FROM 'file:src/main/resources/db/flyway/h2/V1__tabelas_e_dados_iniciais_H2.sql';
 
